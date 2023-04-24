ee8957e0e4ce260f56a61e142cafb4ffd8143986
RasmusJuelN <rasmusn686@hotmail.com>
2023-04-24 13:42:04 +0200

    new shizzledizzle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { BackgroundComponent } from './background/background.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    BackgroundComponent
   ],
   imports: [
     BrowserModule,
