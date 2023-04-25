003e7a5ce75a46a393b4af7e37583007c863847d
RasmusJuelN <rasmusn686@hotmail.com>
2023-04-25 13:52:27 +0200

    loginpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { BackgroundComponent } from './background/background.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    BackgroundComponent
   ],
   imports: [
     BrowserModule,
