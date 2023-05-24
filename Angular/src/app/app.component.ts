4c73e6cfc11675046c0fa9186ec74173623accbe
PurpleGita <rasmus.munk01@gmail.com>
2023-05-24 09:42:49 +0200

    navbar logo gone

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -10,12 +10,26 @@ import { Navigation, NavigationEnd, Router } from '@angular/router';
 })
 export class AppComponent {
   title = 'TempoTune';
-
+  showLogo: boolean = true;
 
   constructor(public router: Router) {
     this.router.navigate(['/main'])
     //this.router.navigate(['/login'])
 
+<<<<<<< HEAD
+=======
+  constructor(private router: Router) {
+    this.router.navigate(['/login'])
+    router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+        if (val.url == '/login' || val.url == '/register') {
+          this.showLogo = true;
+        } else {
+          this.showLogo = false;
+        }
+      }
+    });
+>>>>>>> 6b804d3f8c5ffe37270ba0862f8a107fa2f88bbd
   }
 
   // route for button: ((click)="redirect in the html button")
