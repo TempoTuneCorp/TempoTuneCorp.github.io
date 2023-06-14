8b8e512361f038477416aa0ee3624e972e3a665c
Oscar Staahl <staahl2468@gmail.com>
2023-06-14 08:41:20 +0200

    music player kind of working

--- Angular/src/app/music/music.component.ts
+++ Angular/src/app/music/music.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { Track } from '../models/track.model'
+import { DOCUMENT } from '@angular/common'
 
 @Component({
   selector: 'app-music',
@@ -6,5 +8,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./music.component.css']
 })
 export class MusicComponent {
-  tracks: string[] = ['Woo123123hoo', 'WooYeah', 'YeahYeah', 'YeahWoo' ];
+  constructor(@Inject(DOCUMENT) document: Document){
+    
+  }
+
+  setCurrentSong(track: Track) {
+    (<HTMLAudioElement>document.getElementById('player')).src = track.Path;
+    (<HTMLAudioElement>document.getElementById('player')).play();
+  }
+
+  
+
+  tracks: Track[] = [
+    {
+      Title: "poohead",
+      Path: "assets\\TestAudio\\Free_Test_Data_1MB_MP3.mp3",
+      Album: "WooYeah",
+      Artist: "Rasmus"
+    },
+    {
+      Title: "Dumme",
+      Path: "assets\\TestAudio\\Hvad er det farligste dyr i verden.mp3",
+      Album: "WooYeah",
+      Artist: "Rasmus"
+    },
+    {
+      Title: "Grimme",
+      Path: "assets\\TestAudio\\Mcdonalds Idioten.mp3",
+      Album: "WooYeah",
+      Artist: "Rasmus"
+    },
+    {
+      Title: "Rasmus",
+      Path: "assets\\TestAudio\\Steen med det ekstra ben.mp3",
+      Album: "WooYeah",
+      Artist: "Rasmus"
+    }
+  ];
+
+  
+  
 }
