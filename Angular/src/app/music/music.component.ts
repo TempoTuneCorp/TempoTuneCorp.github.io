3b0dcf106c660ef83471956d89a308c4a3bc3a62
Oscar Staahl <staahl2468@gmail.com>
2023-06-22 08:49:21 +0200

    progress bar workeskeskekskd

--- Angular/src/app/music/music.component.ts
+++ Angular/src/app/music/music.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject } from '@angular/core';
 import { Track } from '../models/track.model'
 import { DOCUMENT } from '@angular/common'
 import { ExpressionType } from '@angular/compiler';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-music',
@@ -70,6 +71,8 @@ export class MusicComponent {
     const audio = (<HTMLAudioElement>document.getElementById('player'))
     const timer = (<HTMLParagraphElement>document.getElementById('timer'))
     const endTimer = (<HTMLParagraphElement>document.getElementById('end-timer'))
+    const progress = (<HTMLDivElement>document.getElementById('progress'))
+    const bar = (<HTMLDivElement>document.getElementById('bar'))
 
     audio.addEventListener('timeupdate', () => {
       const minutes = Math.floor(audio.currentTime / 60);
@@ -78,6 +81,31 @@ export class MusicComponent {
       const secondsDuration = Math.floor(audio.duration % 60);
       timer.innerText = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
       endTimer.innerText = `${minutesDuration}:${secondsDuration < 10 ? '0' : ''}${secondsDuration}`;
+    });
+
+    audio.addEventListener('timeupdate', () => {
+      const percent = (audio.currentTime / audio.duration * 100);
+      progress.style.width = `${percent}%`;
+    });
+
+    bar.addEventListener("click", function(event) {
+      var rect = bar.getBoundingClientRect();
+      var x = event.clientX - rect.left;
+      var y = event.clientY - rect.top;
+
+      console.log("Coordinates: (X: " + x + ", y: " + y + ")");
+
+      // får fat i bredden på baren, og gemmer den som en int i currentWidth.
+      var style = window.getComputedStyle(bar);
+      var currentWidth = parseInt(style.getPropertyValue('width'));
+      console.log(currentWidth);
+
+      // regner procent ud fra hvor du trykker på linjen.
+      const percent = (x / currentWidth * 100);
+      console.log(percent);
+
+      audio.currentTime = (audio.duration*(percent/100));
+
     });
   }
 
