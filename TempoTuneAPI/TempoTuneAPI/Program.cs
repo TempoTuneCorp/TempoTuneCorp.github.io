9de6393a736c99781fba76a711793fed25d61c3a
Administrator <Administrator@WIN-6KTC50PHDF8>
2023-09-25 23:13:11 -0700

    some changed

--- TempoTuneAPI/TempoTuneAPI/Program.cs
+++ TempoTuneAPI/TempoTuneAPI/Program.cs
@@ -45,11 +45,14 @@ builder.Services.AddAuthentication(x =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
